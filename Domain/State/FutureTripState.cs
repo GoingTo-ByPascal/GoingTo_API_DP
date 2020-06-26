7f5d8ba60f97475f34e541e0b2211de3c2c16361
IPorteniu <lporteniu@gmail.com>
2020-06-26 13:06:12 -0500

    implementacion parcial del state

---
+++ Domain/State/FutureTripState.cs
@@ -0,0 +1,22 @@
+﻿using GoingTo_API_DP.Domain.Model;
+using GoingTo_Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoingTo_API_DP.Domain
+{
+    public class FutureTripState : IState<Trip>
+    {
+        public void Future(Trip trip)
+        {
+            throw new Exception("The trip is already in the future");
+        }
+
+        public void Past(Trip trip)
+        {
+            trip.Past();
+        }
+    }
+}
---
+++ Domain/State/PastTripState.cs
@@ -0,0 +1,22 @@
+﻿using GoingTo_API_DP.Domain.Model;
+using GoingTo_Library;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace GoingTo_API_DP.Domain
+{
+    public class PastTripState : IState<Trip>
+    {
+        public void Future(Trip trip)
+        {
+            trip.Future();
+        }
+
+        public void Past(Trip trip)
+        {
+            throw new Exception("The trip is already in the past");
+        }
+    }
+}
